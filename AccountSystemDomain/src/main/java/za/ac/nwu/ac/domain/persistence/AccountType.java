3bf6fb5c6690dc8c2381b51c072d9d90aec7e7aa
Victoria <vicmbedzi@gmail.com>
2021-10-07 19:59:03 +0200

    version of all work since 23 Aug

---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
@@ -0,0 +1,100 @@
+package za.ac.nwu.ac.domain.persistence;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "ACCOUNT_TYPE", schema = "HR")
+public class AccountType implements Serializable{
+    private static final long serialVersionUID = 3833725316797154577L;
+
+    private Long accountTypeId;
+    private String mnemonic;
+    private String accountTypeName;
+    private LocalDate creationDate;
+
+    private Set<AccountUserTransaction> accountUserTransactions;
+
+    public AccountType(Long accountTypeId, String mnemonic, String accountTypeName, LocalDate creationDate) {
+        this.accountTypeId = accountTypeId;
+        this.mnemonic = mnemonic;
+        this.accountTypeName = accountTypeName;
+        this.creationDate = creationDate;
+    }
+
+    public AccountType()
+    {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ACCOUNT_TYPE_ID", nullable = false)
+    public Long getAccountTypeId() {
+        return accountTypeId;
+    }
+
+    public void setAccountTypeId(Long accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
+
+    @Column(name = "MNEMONIC", nullable = false)
+    public String getMnemonic() {
+        return mnemonic;
+    }
+
+    public void setMnemonic(String mnemonic) {
+        this.mnemonic = mnemonic;
+    }
+
+    @Column(name = "ACCOUNT_TYPE_NAME", nullable = false)
+    public String getAccountTypeName() {
+        return accountTypeName;
+    }
+
+    public void setAccountTypeName(String accountTypeName) {
+        this.accountTypeName = accountTypeName;
+    }
+
+    @Column(name = "CREATION_DATE", nullable = false)
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @OneToMany(targetEntity = AccountUserTransaction.class, fetch = FetchType.LAZY, mappedBy = "accountType", orphanRemoval = true, cascade = CascadeType.PERSIST)
+    public Set<AccountUserTransaction> getAccountUserTransactions(){
+        return accountUserTransactions;
+    }
+
+    public void setAccountUserTransactions(Set<AccountUserTransaction> accountUserTransactions){
+        this.accountUserTransactions = accountUserTransactions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountType that = (AccountType) o;
+        return Objects.equals(accountTypeId, that.accountTypeId) && Objects.equals(mnemonic, that.mnemonic) && Objects.equals(accountTypeName, that.accountTypeName) && Objects.equals(creationDate, that.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountTypeId, mnemonic, accountTypeName, creationDate);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountType{" +
+                "accountTypeId=" + accountTypeId +
+                ", mnemonic='" + mnemonic + '\'' +
+                ", accountTypeName='" + accountTypeName + '\'' +
+                ", creationDate=" + creationDate +
+                '}';
+    }
+}
---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountUserTransaction.java
@@ -0,0 +1,103 @@
+package za.ac.nwu.ac.domain.persistence;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "ACCOUNT_TRANSACTION", schema = "HR")
+public class AccountUserTransaction implements Serializable{
+
+    private static final long serialVersionUID = -7055277179559220101L;
+
+    private Long transactionID;
+    private AccountType accountType;
+    private Long memberId;
+    private Long amount;
+    private LocalDate transactionDate;
+
+    public AccountUserTransaction()
+    {}
+
+    public AccountUserTransaction(Long transactionID, AccountType accountType, Long memberId, Long amount, LocalDate transactionDate) {
+        this.transactionID = transactionID;
+        this.accountType = accountType;
+        this.memberId = memberId;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "TRANSACTION_ID", nullable = false)
+    public Long getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(Long transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ACCOUNT_TYPE_ID")
+    public AccountType getAccountType()
+    {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType){
+        this.accountType = accountType;
+    }
+
+    @Column(name = "MEMBER_ID", nullable = false)
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    @Column(name = "AMOUNT", nullable = false)
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    @Column(name = "TRANSACTION_DATE", nullable = false)
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountUserTransaction that = (AccountUserTransaction) o;
+        return Objects.equals(transactionID, that.transactionID) && Objects.equals(accountType, that.accountType) && Objects.equals(memberId, that.memberId) && Objects.equals(amount, that.amount) && Objects.equals(transactionDate, that.transactionDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionID, accountType, memberId, amount, transactionDate);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountUserTransaction{" +
+                "transactionID=" + transactionID +
+                ", accountTypeId=" + accountType +
+                ", memberId=" + memberId +
+                ", amount=" + amount +
+                ", transactionDate=" + transactionDate +
+                '}';
+    }
+}
